2bf6f50f46883a061c922021b6fc8e0e93216776
momonaebuy <momona@angineers.com>
2019-12-11 18:18:02 +0700

    sellerupload/search/location set

--- src/App.js
+++ src/App.js
@@ -5,26 +5,39 @@ import Footer from'./components/layout/Footer';
 import Products from './components/Pages/Products';
 import AboutUs from './components/Pages/AboutUs';
 import ContactUs from './components/Pages/ContactUs';
+<<<<<<< HEAD
 
 import SellerUpload from './components/Pages/SellerUpload';
 import Map from './components/Pages/Map';
 
+=======
+import SignIn from './components/auth/SignIn';
+import SignUpSeller from './components/auth/SignUpSeller';
+import SignUpBuyer from './components/auth/SignUpBuyer';
+import Home from './components/Pages/HomePage'
+>>>>>>> master
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
         <Switch>
-          <Route exact path='/'component={Products} />
-          {/* <Route path='/products'component={Home}/>
-          <Route path='/products'component={MyChat}/>
-  <Route path='/'component={Profile}/>*/}
+        <Route path='/products'component={Products} />
+          <Route exact path='/'component={Home}/>
+          {/* <Route path='/products'component={MyChat}/>
+  <Route path='/'component={Profile}/> */}
           <Route path='/contactus'component={ContactUs}/>
           <Route path='/aboutus'component={AboutUs}/> 
+<<<<<<< HEAD
           
           <Route path='/sellerupload'component={SellerUpload}/>
           <Route path='/map'component={Map}/>
      
+=======
+          <Route path='/signin'component={SignIn}/> 
+          <Route path='/signupBuyer'component={SignUpBuyer}/> 
+          <Route path='/signupSeller'component={SignUpSeller}/>  
+>>>>>>> master
         </Switch>
         <Footer/>
     </BrowserRouter>
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,24 @@
 import productReducer from './ProductReducer';
+<<<<<<< HEAD
 import {firestoreReducer} from 'redux-firestore';
+=======
+import authReducer from './authReducer'
+import {firestoreReducer} from 'redux-firestore'
+import {firebaseReducer} from 'react-redux-firebase'
+>>>>>>> master
 import {combineReducers} from 'redux';
 import "firebase/storage";
 const rootReducer=combineReducers({
+<<<<<<< HEAD
     products:productReducer,
     firestore:firestoreReducer
+=======
+    auth:authReducer,
+    products:productReducer,
+    firestore:firestoreReducer,
+    firebase:firebaseReducer
+ 
+  
+>>>>>>> master
 });
 export default rootReducer;
