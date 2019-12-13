73d70470561d1c235f640868366a19c32515c6d4
yddya <nebiat@anginners.com>
2019-12-13 16:27:09 +0200

    updated comment and chat

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,20 +2,14 @@ import productReducer from './ProductReducer';
 import authReducer from './authReducer'
 import {firestoreReducer} from 'redux-firestore'
 import {firebaseReducer} from 'react-redux-firebase'
-import {firestoreReducer} from 'redux-firestore';
-import {combineReducers} from 'redux';
- 
+import {combineReducers} from 'redux'; 
 import commentReducer from './commentReducer'
 import chatReducer from './chatReducer'
-
-const rootReducer=combineReducers({
-    chat:chatReducer,
-    comment:commentReducer,
- 
 import "firebase/storage";
 const rootReducer=combineReducers({
      auth:authReducer,
- 
+     chat:chatReducer,
+     comment:commentReducer, 
     products:productReducer,
     firestore:firestoreReducer,
     firebase:firebaseReducer
