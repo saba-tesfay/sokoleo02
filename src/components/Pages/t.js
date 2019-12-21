9a6abcb919a544efd67cea4e52d304598bcfd80f
momonaebuy <momona@angineers.com>
2019-12-21 15:36:19 +0700

    finishing up

--- src/components/Pages/t.js
+++ src/components/Pages/t.js
@@ -1,81 +1,50 @@
 import React, { Component } from "react";
 import { withScriptjs, withGoogleMap, GoogleMap, Circle,Marker } from "react-google-maps";
 import Autocomplete from 'react-google-autocomplete';
-const Map = withScriptjs(
-    withGoogleMap(props => (
-        <GoogleMap
-            defaultZoom={12}
-            defaultCenter={{ lat: -34.397, lng: 150.644 }}
-            onClick={e => props.onMapClick(e)}
-        >
-         {console.log("belew",props.mark)}
-            {props.marks.map((mark, index) => <Marker key={index} 
-      
-            // position={mark}
-            position={{ lat: mark.lat(),  lng: mark.lng() }}
-            // position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
-          
-             />)}
-            {/* <Autocomplete
-               style={{
-                width: '50%',
-                height: '40px',
-                paddingLeft: '16px',
-                marginTop: '10px',
-                marginBottom: '100px'
-               }}
-              
-            //    onPlaceSelected={ this.onPlaceSelected }
-               types={['(regions)']}
-              /> */}
-        </GoogleMap>
-    ))
-);
-
+import Geocode from "react-geocode";
+ 
+// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey("AIzaSyCd5GSrdhkRjDu53HCBVL7fh5QXa1-gIBE");
+ 
+// set response language. Defaults to english.
+Geocode.setLanguage("en");
+ 
+// set response region. Its optional.
+// A Geocoding request with region=es (Spain) will return the Spanish city.
+Geocode.setRegion("es");
+ 
+// Enable or disable logs. Its optional.
+Geocode.enableDebug();
+ 
+// Get address from latidude & longitude.
+// Geocode.fromLatLng("48.8583701", "2.2922926").then(
+//   response => {
+//     const address = response.results[0].formatted_address;
+//     console.log(address);
+//   },
+//   error => {
+//     console.error(error);
+//   }
+// );
+ 
+// // Get latidude & longitude from address.
+// Geocode.fromAddress("castle").then(
+//   response => {
+//     const { lat, lng } = response.results[0].geometry.location;
+//     console.log("belew",lat, lng);
+//   },
+//   error => {
+//     console.error(error);
+//   }
+// );
 class R extends Component {
-    state = {
-        marks: [],
-        markerPosition: {
-            lat:null,
-            lng:null
-        }
-     
-    };
-
-    setMark = e => {
-            let latValue= e.latLng.lat()
-            let  lngValue=e.latLng.lng()
-         console.log("really", [e.latLng.lat(),e.latLng.lng()])
-       this.setState({ marks: [...this.state.marks,e.latLng],
-            markerPosition: {
-                lat: latValue,
-                lng: lngValue
-               }
-        });
-    };
-
-    deleteMarkS = () => {
-        this.setState({
-            marks: []
-            
-        });
-    };
+  
 
     render() {
-        const { marks } = this.state;
-        console.log("eshi",{marks})
+      
         return (
             <div>
-                <button onClick={this.deleteMark}>DELETE MARKS</button>
-                <Map
-                    googleMapURL="http://maps.googleapis.com/maps/api/js?key=AIzaSyCd5GSrdhkRjDu53HCBVL7fh5QXa1-gIBE"
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={<div style={{ height: `400px` }} />}
-                    mapElement={<div style={{ height: `100%` }} />}
-                    onMapClick={this.setMark}
-                    center={{lat: -0.45275,lng: 39.64601}}
-                    marks={marks}
-                />;
+        <h2> </h2>
             </div>
         );
     }
