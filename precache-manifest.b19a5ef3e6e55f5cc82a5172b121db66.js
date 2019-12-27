eef3700595d04d0adf925e65fdccdfe81feac5cc
saba-tesfay <saba@angineers.com>
2019-12-27 23:17:02 +0700

    Updates

---
+++ precache-manifest.b19a5ef3e6e55f5cc82a5172b121db66.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4741e58bc136bee722fbeb570f68043b",
+    "url": "/sokoleo02/index.html"
+  },
+  {
+    "revision": "df20520d6e2e6b76417e",
+    "url": "/sokoleo02/static/css/2.ac1b96a0.chunk.css"
+  },
+  {
+    "revision": "a40ffc589cae3311c8ca",
+    "url": "/sokoleo02/static/css/main.5db81220.chunk.css"
+  },
+  {
+    "revision": "df20520d6e2e6b76417e",
+    "url": "/sokoleo02/static/js/2.9802ec48.chunk.js"
+  },
+  {
+    "revision": "a40ffc589cae3311c8ca",
+    "url": "/sokoleo02/static/js/main.8fb9432d.chunk.js"
+  },
+  {
+    "revision": "9c512ee27697e6eb6645",
+    "url": "/sokoleo02/static/js/runtime-main.c8d5eaa8.js"
+  },
+  {
+    "revision": "c1a67a3916dd7716fb7919fc90ae065d",
+    "url": "/sokoleo02/static/media/about.c1a67a39.jpg"
+  },
+  {
+    "revision": "f747deefb0dff7662b1b627d80aa6db3",
+    "url": "/sokoleo02/static/media/bg_1.f747deef.jpg"
+  },
+  {
+    "revision": "dba5ba6ed8474bac599c68207b13796f",
+    "url": "/sokoleo02/static/media/bg_2.dba5ba6e.jpg"
+  },
+  {
+    "revision": "637b8f8baf6e95478ceb6322eeb2e370",
+    "url": "/sokoleo02/static/media/bg_3.637b8f8b.jpg"
+  },
+  {
+    "revision": "638a16ab279c32726cd77a3eb2d4cfa2",
+    "url": "/sokoleo02/static/media/category-1.638a16ab.jpg"
+  },
+  {
+    "revision": "e975c99d7863606d5342bee409a11f62",
+    "url": "/sokoleo02/static/media/category-2.e975c99d.jpg"
+  },
+  {
+    "revision": "9a73352f7a604700dd7474a5ca01b5cb",
+    "url": "/sokoleo02/static/media/category-3.9a73352f.jpg"
+  },
+  {
+    "revision": "600671909d55d8db086ae2962e5c75c2",
+    "url": "/sokoleo02/static/media/category-4.60067190.jpg"
+  },
+  {
+    "revision": "22aa382b7d25e545c7292c12c41fc5bc",
+    "url": "/sokoleo02/static/media/category.22aa382b.jpg"
+  },
+  {
+    "revision": "47e2c88c847a0b1a817ca6636cfe63dc",
+    "url": "/sokoleo02/static/media/product_1.47e2c88c.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/sokoleo02/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/sokoleo02/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/sokoleo02/static/media/revicons.2feb69cc.eot"
+  }
+]);
