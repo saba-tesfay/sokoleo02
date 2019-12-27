ca04e072447340c578d8c4a727ea95b5b32c80c0
saba-tesfay <saba@angineers.com>
2019-12-27 17:45:37 +0700

    sokolnileo=>sokoleo

--- src/components/layout/SignedIn.js
+++ src/components/layout/SignedIn.js
@@ -11,7 +11,7 @@ const SignedIn = (props) => {
         <AboveNavbar/>     
 <nav  class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 <div class="container">
-<a class="navbar-brand"  href="./"><i  class="ion-ios-home pr-2"/>Sokonileo</a>
+<a class="navbar-brand"  href="./"><i  class="ion-ios-home pr-2"/>Sokoleo</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 <span class="oi oi-menu"></span> Menu</button>
 <div class="collapse navbar-collapse" id="ftco-nav">
